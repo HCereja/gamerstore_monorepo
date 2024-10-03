145588b84f70b7325cc9afc508fd9130c7afa7a1
Henrique <cerejahenrique@gmail.com>
2024-10-03 18:55:22 -0300

    feat: criar product.controller

--- apps/backend/src/product/product.controller.ts
+++ apps/backend/src/product/product.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ProductPrisma } from './product.prisma';
+import { Product } from '@gstore/core';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly repo: ProductPrisma) {}
+
+  @Post()
+  save(@Body() produto: Product): Promise<void> {
+    return this.repo.save(produto);
+  }
+
+  @Get()
+  getAll(): Promise<Product[]> {
+    return this.repo.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string): Promise<Product> {
+    return this.repo.getById(+id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.repo.delete(+id);
+  }
+}
