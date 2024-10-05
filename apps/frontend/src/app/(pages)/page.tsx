5ac0380d59c7352752a6bd858a4d24ab8697f94e
Henrique <cerejahenrique@gmail.com>
2024-10-05 18:27:25 -0300

    feat: criar componente ProductFilter

--- apps/frontend/src/app/(pages)/page.tsx
+++ apps/frontend/src/app/(pages)/page.tsx
@@ -1,8 +1,10 @@
+import FilterProducts from "@/components/product/FilterProducts";
 import ProductList from "@/components/product/ProductList";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-5 py-10 container">
+      <FilterProducts />
       <ProductList />
     </div>
   );
